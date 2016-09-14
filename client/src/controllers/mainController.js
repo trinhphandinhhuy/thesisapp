011c7f00b50e84a61407f96a630352899071bc6a
Huy Trinh <trinhphandinhhuy@gmail.com>
2016-09-14 11:37:06 +0300

    General restructure

---
+++ client/src/controllers/mainController.js
@@ -0,0 +1,8 @@
+
+
+lamkRTC.controller("mainController", function ($scope, $location) {
+    $scope.hostname = $location.absUrl();
+    $scope.createRoom = function () {
+        $location.path("/" + $scope.roomName);
+    };
+});
