009b264c8e48874ee4a8fcc4d8860f6770277d73
Huy Trinh <trinhphandinhhuy@gmail.com>
2016-09-18 17:51:33 +0300

    Change Chrome to Firefox Launcher

--- karma.conf.js
+++ karma.conf.js
@@ -60,13 +60,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome_no_sandbox'],
-    customLaunchers: {
-      Chrome_no_sandbox: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    },
+    browsers: ['Firefox'],
 
 
     // Continuous Integration mode
